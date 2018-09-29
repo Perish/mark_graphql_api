9d409f9b5856b779a37a029c312018bd112b4fab
xc <xuchong@xcdeMacBook-Pro.local>
2018-09-29 23:18:14 +0800

    add guardian package

--- mix.exs
+++ mix.exs
@@ -42,8 +42,10 @@ defmodule MarkGraphqlApi.Mixfile do
       {:comeonin, "~> 4.0"},
       {:argon2_elixir, "~> 1.2"},
       {:absinthe, "~> 1.4"},
+      {:absinthe_phoenix, "~> 1.4"},
       {:absinthe_plug, "~> 1.4"},
-      {:absinthe_ecto, "~> 0.1.3"}
+      {:absinthe_ecto, "~> 0.1.3"},
+      {:guardian, "~> 1.1"}
     ]
   end
 
