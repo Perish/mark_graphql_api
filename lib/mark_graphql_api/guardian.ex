74e13341d4ea33fe0da619ac0c83f8fae8706c11
xc <xuchong@xcdeMacBook-Pro.local>
2018-09-29 23:20:52 +0800

    implement guardian

---
+++ lib/mark_graphql_api/guardian.ex
@@ -0,0 +1,24 @@
+defmodule MarkGraphqlApi.Guardian do
+  use Guardian, otp_app: :mark_graphql_api
+
+  alias MarkGraphqlApi.Accounts
+
+  def subject_for_token(resource, _claims) do
+    sub = to_string(resource.id)
+    {:ok, sub}
+  end
+
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
+  def resource_from_claims(claims) do
+    id = claims["sub"]
+    resource = Accounts.get_user!(id)
+    {:ok, resource}
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+end
